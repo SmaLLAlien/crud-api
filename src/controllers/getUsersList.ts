6548d80020772f2c05e382bf4e702df6f2c46b76
Vladyslav Bilousov <vladyslav_bilousov@epam.com>
2022-06-16 18:35:15 +0300

    feat: changed controller name

--- src/controllers/getUsersList.ts
+++ src/controllers/getUsersList.ts
@@ -3,7 +3,7 @@ import * as db from '../utils/db';
 import {IRequest} from "../config/interfaces";
 import {handleDbError} from "../utils/handleDbError";
 
-export const getUsers = async (req: IRequest, res: http.ServerResponse) => {
+export const getUsersList = async (req: IRequest, res: http.ServerResponse) => {
     try {
         const userList = await db.getUsersList();
         res.end(JSON.stringify(userList));
--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,5 +1,5 @@
 import {createUser} from "../controllers/createUser";
-import {getUsers} from "../controllers/getUsers";
+import {getUsersList} from "../controllers/getUsersList";
 import {updateUser} from "../controllers/updatUser";
 import {deleteUser} from "../controllers/deleteUser";
 import {getUser} from "../controllers/getUser";
@@ -10,7 +10,7 @@ export const STATIC_ROUTES = {
     },
     GET: {
         '': '<h1>welcome to homepage</h1><hr>',
-        '/api/users': getUsers,
+        '/api/users': getUsersList,
     }
 }
 
